b69922a7ab72f1ca5d0ae2b2660cf5898dc58b6f
Pandemonium1986 <15736601+Pandemonium1986@users.noreply.github.com>
2021-11-14 12:45:14 +0100

    feat: update config to declare LANG env variables

--- provisioner/shell/centos8/config.sh
+++ provisioner/shell/centos8/config.sh
@@ -6,4 +6,10 @@ printf "ZONE=\"Europe/Paris\"\n" > /etc/sysconfig/clock
 restorecon -Rv /etc/localtime
 timedatectl set-timezone Europe/Paris
 
+# Config LANG
+printf "LC_ALL=en_US.UTF-8" >> /etc/environment
+printf "LANG=en_US.UTF-8" >> /etc/environ
+printf "LANGUAGE=en_US.UTF-8" >> /etc/environ
+printf "PYTHONIOENCODING=utf8" >> /etc/environ
+
 # sshd
