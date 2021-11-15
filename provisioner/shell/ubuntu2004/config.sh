b39515a5bc03d47c6323dc903c02386e7ccd6d8b
Pandemonium1986 <15736601+Pandemonium1986@users.noreply.github.com>
2021-11-15 18:45:20 +0100

    fix: network config in ubuntu2004

--- provisioner/shell/ubuntu2004/config.sh
+++ provisioner/shell/ubuntu2004/config.sh
@@ -9,3 +9,22 @@ printf "LC_ALL=en_US.UTF-8\n" >> /etc/environment
 printf "LANG=en_US.UTF-8\n" >> /etc/environment
 printf "LANGUAGE=en_US.UTF-8\n" >> /etc/environment
 printf "PYTHONIOENCODING=utf8\n" >> /etc/environment
+
+# Network
+cat <<-EOF > /etc/netplan/01-netcfg.yaml
+network:
+  version: 2
+  renderer: networkd
+  ethernets:
+    eth0:
+      dhcp4: true
+      dhcp6: false
+      optional: true
+      nameservers:
+        addresses: [9.9.9.9, 149.112.112.112]
+EOF
+
+# Apply the network plan configuration.
+netplan generate
+
+sed -i -e "s/DNS=.*/DNS=9.9.9.9 149.112.112.112/g" /etc/systemd/resolved.conf
